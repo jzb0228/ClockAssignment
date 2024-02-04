f6ded484c437a0cd486d15f592a3aadd88360c2b
Justin Baik <jzb0228@gmail.com>
2024-02-03 17:03:58 -0800

    Datetime label and code added

--- ClockAssignment/ViewController.swift
+++ ClockAssignment/ViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Label showing current date
+    @IBOutlet weak var dateTimeLabel: UILabel!
+    //shows current datetime in format "Sat, 02 February 2024, 05:03:00 PM"
+    fileprivate func startTime() {
+        
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (_) in
+            let date = Date() + 40000
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "EEE, dd MMM yyyy hh:mm:ss a"
+            let currentTime = dateFormatter.string(from: date)
+            self.dateTimeLabel.text = currentTime
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startTime()
     }
 
 
